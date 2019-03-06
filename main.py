1c61a1896ccadd0cdb8d67867154472108897356
Yamaguchi, Takuya <yamaguchi.takuya2015@jp.panasonic.com>
2019-03-06 15:41:33 +0900

    modifi class and dummy func

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
 import tweepy
 
-# set tweeter key from ENV
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_TOKEN = ''
-ACCESS_SECRET = ''
+class SekaokuTwitter():
+    def __init__(self):
+        # set tweeter key from ENV
+        #TODO
+        CONSUMER_KEY = ''
+        CONSUMER_SECRET = ''
+        ACCESS_TOKEN = ''
+        ACCESS_SECRET = ''
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+        self.api = tweepy.API(auth)
 
-api = tweepy.API(auth)
+    def is_retweeted(self, tweet):
+        #TODO
+        return True
 
-tweets = api.search(q='"#SEKAINOOKUDA"', lang='ja', result_type='recent',count=12)
-user_ids = []
+    def is_included_img(self, tweet):
+        #TODO
+        return True
 
-for tweet in tweets:
-    user_id = tweet.user.id
-    screen_name = tweet.user.screen_name
-    user_ids.append(user_id)
+    def retweet(self, tweet):
+        #TODO
+        pass
 
-print(user_ids)
+    def save_image(self, tweet):
+        #TODO
+        if not self.is_included_img(tweet):
+            return False
+        return True
+
+    def main(self):
+        num_tweets = 25
+        tweets = self.api.search(q='"#せかいのおくだ"',
+                                 lang='ja',
+                                 result_type='recent',
+                                 count=num_tweets)
+        for tweet in tweets:
+            if self.is_retweeted(tweet):
+                break
+            self.save_image(tweet)
+            self.retweet(tweet)
+
+
+if __name__ == '__main__':
+    sekaoku_tw = SekaokuTwitter()
+    sekaoku_tw.main()
+
